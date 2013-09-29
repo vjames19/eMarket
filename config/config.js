a9594bff106ea0fbe48ff856a6aab147fc743602
Victor J. Reventos <victor.reventos1@gmail.com>
2013-09-29 15:39:45 -0400

    Fixed environment configurations.

--- config/config.js
+++ config/config.js
@@ -5,5 +5,5 @@ var _ = require('underscore');
 // Load app configuration
 
 module.exports = _.extend(
-    require(__dirname + '/../config/env/all.js'),
-    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {});
+    require('./env/all.js'),
+    require('./env/' + process.env.NODE_ENV + '.json') || {});
