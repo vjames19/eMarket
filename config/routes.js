37be8c5891186dec31d418c763b664fcc8970352
samy100 <tech_boy@outlook.com>
2013-09-30 12:53:02 -0400

    Fixing jsHint errors.

--- config/routes.js
+++ config/routes.js
@@ -48,4 +48,7 @@ module.exports = function(app, passport, auth) {
   app.get('/api/products/:productId/bids/:bidId', products.readProductBid);
   app.put('/api/products/:productId/bids/:bidId', products.updateProductBid);
   app.del('/api/products/:productId/bids/:bidId', products.deleteProductBid);
+
+  //
+
 };
