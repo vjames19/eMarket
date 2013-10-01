be885f57f252a8c544e112b187fa87605c282a1a
Victor J. Reventos <victor.reventos1@gmail.com>
2013-09-30 23:19:40 -0400

    Removed unused middleware.

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -5,31 +5,7 @@
  */
 exports.requiresLogin = function(req, res, next) {
   if(!req.isAuthenticated()) {
-    return res.send(401, 'User is not authorized');
+    return res.jsonp(401, 'User is not authorized');
   }
   next();
-};
-
-/**
- * User authorizations routing middleware
- */
-exports.user = {
-  hasAuthorization: function(req, res, next) {
-    if(req.profile.id !== req.user.id) {
-      return res.send(401, 'User is not authorized');
-    }
-    next();
-  }
-};
-
-/**
- * Article authorizations routing middleware
- */
-exports.article = {
-  hasAuthorization: function(req, res, next) {
-    if(req.article.user.id !== req.user.id) {
-      return res.send(401, 'User is not authorized');
-    }
-    next();
-  }
 };
