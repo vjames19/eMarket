d360eef684faff3bc789391feb475bd437da4864
samy100 <tech_boy@outlook.com>
2013-10-01 22:31:56 -0400

    changing logic to remove returns.

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -4,16 +4,17 @@ var admins = require('../../app/controllers/admins');
 /**
  * Generic require login routing middleware
  */
-exports.requiresLogin = function(req, res, next) {
-  if(!req.isAuthenticated()) {
-    return res.jsonp(401, 'User is not authorized');
+exports.requiresLogin = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.jsonp(401, 'User is not authorized');
   }
-  next();
 };
 
 exports.admin = {
-  hasAuthorization: function(req, res, next) {
-    if(admins.isAdmin(req.user)) {
+  hasAuthorization: function (req, res, next) {
+    if (admins.isAdmin(req.user)) {
       next();
     } else {
       res.jsonp(401, {message: req.user.username + ' is not an Administrator'});
