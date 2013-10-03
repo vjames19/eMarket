8232123eb9ec3267d6ef767557db14208db8136a
samy100 <tech_boy@outlook.com>
2013-10-03 00:24:36 -0400

    Removing Webstorm Comments

--- public/js/directives/registerUser.js
+++ public/js/directives/registerUser.js
@@ -1,10 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Eduardo
- * Date: 10/2/13
- * Time: 9:40 PM
- * To change this template use File | Settings | File Templates.
- */
 'use strict';
 
 angular.module('eMarketApp')
