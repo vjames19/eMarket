73550f3805e3d62e246ed378ec27528f4a0ad441
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-04 15:17:30 -0400

    Angularized notifications.

--- public/js/directives/notifications.js
+++ public/js/directives/notifications.js
@@ -6,6 +6,17 @@ angular.module('eMarketApp')
         templateUrl: 'views/notifications.html',
         restrict: 'E',
         scope: {},
-        replace: true
+        replace: true,
+        controller: function($scope) {
+          $scope.notifications = [
+            {state: 'Unread', message: 'Asus market...'},
+            {state: 'Read', message: 'How long is it going to take'}
+          ];
+        },
+        link: function(scope, elem) {
+          scope.setMessage = function(notification) {
+            $(elem[0]).find('#notificationMessage').text(notification.message);
+          }
+        }
       };
     });
