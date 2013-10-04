f67a58ad4d46bbe74195c590f6cae719621eb0d5
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-04 11:04:57 -0400

    Angularized itemView

--- public/js/directives/itemView.js
+++ public/js/directives/itemView.js
@@ -6,6 +6,29 @@ angular.module('eMarketApp')
         templateUrl: 'views/itemView.html',
         restrict: 'E',
         scope: {},
-        replace: true
+        replace: true,
+        controller: function($scope) {
+          $scope.item = {
+            productId: 2,
+            productSellerId: 9,
+            productCategory: 'computers',
+            productName: 'alienware',
+            productBuyItNowPrice: 1000.99,
+            productStartingBidPrice: 500.99,
+            productBidEndDate: '07/07/2008',
+            productShippingPrice: 8.99,
+            productQuantity: 3,
+            productDescription: {
+              productCondition: 'Refurbished',
+              productPicture: '/img/products/users/9/z98gyu.png',
+              productBrand: 'dell',
+              productModel: 'M179385',
+              productDimensions: '19x30x25'
+            },
+            productLocation: 'California, USA'
+          };
+
+          $scope.remainingTime = $scope.item.endDate;
+        }
       };
     });
