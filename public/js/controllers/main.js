a9dc600ee6bcfb5c3cb90b50ba336d1b9e1b627e
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-04 18:32:19 -0400

    Changed itemView to accept an object binding to render its properties.

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('eMarketApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+      $scope.itemToBeViewed = null;
+      // Use to set the item to be viewed
+      $scope.setItem = function(item) {
+        $scope.itemToBeViewed = item;
+      }
+    });
