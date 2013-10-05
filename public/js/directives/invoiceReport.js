b2defb5d99daceffe1c74e0fca58548911aa64bc
samy100 <tech_boy@outlook.com>
2013-10-05 03:38:30 -0400

    Adding invoice Report template page.

---
+++ public/js/directives/invoiceReport.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('eMarketApp')
+    .directive('invoiceReport', function () {
+      return {
+        templateUrl: 'views/invoiceReport.html',
+        restrict: 'E',
+        scope: {},
+        replace: true
+      };
+    });
+
