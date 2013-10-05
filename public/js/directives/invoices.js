2bf1232535269b9fd25c02cef44d38d0f38a405a
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-05 18:01:49 -0400

    Synchronized invoices.

--- public/js/directives/invoices.js
+++ public/js/directives/invoices.js
@@ -1,15 +1,23 @@
 'use strict';
 
 angular.module('eMarketApp')
-    .directive('invoices', function () {
+    .directive('invoices', function(User, Restangular) {
       return {
         templateUrl: 'views/invoices.html',
         restrict: 'E',
         scope: {},
         replace: true,
-        controller: function($scope, Restangular) {
-          // TODO: replace with real user id.
-          $scope.invoices = Restangular.one('api/users', 1).getList('invoices');
+        link: function(scope, elem) {
+          var page = $(elem[0]);
+          var invoiceList = page.find("#invoiceList");
+
+          page.on('pagebeforeshow', function() {
+            scope.invoices = User.me().getList('invoices');
+          });
+
+          page.on('pageshow', function() {
+            invoiceList.listview('refresh');
+          });
         }
       };
     });
