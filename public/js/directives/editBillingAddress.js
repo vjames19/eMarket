ac1680868f15d8c95c754ac55015af0273d011c0
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-05 23:01:05 -0400

    billing address put resource

--- public/js/directives/editBillingAddress.js
+++ public/js/directives/editBillingAddress.js
@@ -1,14 +1,25 @@
 'use strict';
 
 angular.module('eMarketApp')
-    .directive('editBillingAddress', function () {
+    .directive('editBillingAddress', function() {
       return {
         templateUrl: 'views/editBillingAddress.html',
         restrict: 'E',
         scope: {
           billInfo: '='
         },
-        replace: true
+        replace: true,
+        controller: function($scope, User) {
+          $scope.submit = function() {
+            console.log($scope.billInfo);
+            User.me().one('billAddresses', $scope.billInfo.billAddressId).customPUT($scope.billInfo)
+                .then(function(billInfo) {
+                  $scope.billInfo = billInfo;
+                }, function(err) {
+                  alert(err);
+                });
+          }
+        }
       };
     });
 
