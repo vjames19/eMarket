e3d19fda9ed724dfa8e787f31f6384ee8237f355
samy100 <tech_boy@outlook.com>
2013-10-06 16:49:11 -0400

    Fixing bug

--- public/js/services/auth.js
+++ public/js/services/auth.js
@@ -1,33 +1,32 @@
 'use strict';
 
-var _ = require('underscore');
 
-angular.module('eMarketApp').factory('Auth', function($rootScope, $http, User) {
+angular.module('eMarketApp').factory('Auth', function ($rootScope, $http, User) {
   var user = User;
   var isLoggedIn = false;
   var unAuthPaths = ['login-user', 'register', 'forgot-password', 'shopping-cart', 'item-view'];
   return {
-    logIn: function(userData) {
-      $http.post('login', userData).success(function(realUser) {
+    logIn: function (userData) {
+      $http.post('login', userData).success(function (realUser) {
         user.username = realUser.username;
         user.userId = realUser.id;
         $.mobile.changePage('#index-page');
         isLoggedIn = true;
-      }).error(function() {
+      }).error(function () {
             $('#loginError').popup('open');
           });
 
     },
-    logOut: function() {
+    logOut: function () {
       user.username = null;
       user.userId = null;
       isLoggedIn = false;
     },
-    isLoggedIn: function() {
+    isLoggedIn: function () {
       return isLoggedIn;
     },
-    isValidUnAuthPath: function(path) {
-      return path === '' || _.some(unAuthPaths, function(unAuthPath) {
+    isValidUnAuthPath: function (path) {
+      return path === '' || _.some(unAuthPaths, function (unAuthPath) {
         console.log(path, unAuthPath, path.indexOf(unAuthPath) >= 0);
         return path.indexOf(unAuthPath) >= 0;
       });
