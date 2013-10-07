ddeaaee93f606e942ee24ec6cbabd198bdf96cdd
samy100 <tech_boy@outlook.com>
2013-10-07 14:28:22 -0400

    fixing bid to itemview problem

--- public/js/directives/itemView.js
+++ public/js/directives/itemView.js
@@ -9,27 +9,31 @@ angular.module('eMarketApp')
           item: '='
         },
         replace: true,
-        link: function(scope, elem) {
+        link: function (scope, elem) {
           var page = $(elem[0]);
+          var popupItemAddedToCart = page.find('#popupItemAddedToCart');
           var buyItNowButton = page.find('#buy-it-now-button');
           var bidButton = page.find('#place-bid-button');
           var placeBidInput = page.find('#placeBid');
 
-          page.on('pagebeforeshow', function() {
-            if(User.userId === scope.item.productSellerId) {
-              buyItNowButton.addClass('ui-disabled');
-              bidButton.addClass('ui-disabled');
-            } else {
-              bidButton.removeClass('ui-disabled');
-              buyItNowButton.removeClass('ui-disabled');
-            }
-          });
+          if (scope.item.productSellerId) {
+            page.on('pagebeforeshow', function () {
+              if (User.userId === scope.item.productSellerId) {
+                buyItNowButton.addClass('ui-disabled');
+                bidButton.addClass('ui-disabled');
+              } else {
+                bidButton.removeClass('ui-disabled');
+                buyItNowButton.removeClass('ui-disabled');
+              }
+            });
+          }
 
-          scope.addToCart = function() {
+          scope.addToCart = function () {
             // Get the item quantity and multiply it by the price to get the total cost
             scope.item.cost = scope.item.quantity * scope.item.productBuyItNowPrice;
             User.me().all('carts').post(scope.item);
           };
+
 
           scope.placeMinBid = function () {
             placeBidInput.attr('value', scope.item.productCurrentBidPrice + 5);
