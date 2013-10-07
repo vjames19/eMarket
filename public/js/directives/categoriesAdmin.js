f24db19ff0c3d3afaab3ce96bea19703025ec367
samy100 <tech_boy@outlook.com>
2013-10-07 03:33:23 -0400

    adding delete category and user

--- public/js/directives/categoriesAdmin.js
+++ public/js/directives/categoriesAdmin.js
@@ -8,8 +8,20 @@ angular.module('eMarketApp')
         scope: true,
         replace: true,
         link: function (scope, elem) {
+          var category = null;
           var page = $(elem[0]);
           var categoryAdminList = page.find('#categoryAdminList');
+
+          scope.selectedCategory = function (selectedCategory) {
+            category = selectedCategory;
+          };
+
+          scope.deleteCategory = function () {
+            $.mobile.loading('show');
+            Restangular.one('api/categories', category.categoryId).remove();
+            $.mobile.loading('hide');
+            scope.refreshDom();
+          };
 
           page.on('pagebeforeshow', function () {
             scope.categories = Restangular.all('api/categories').getList();
