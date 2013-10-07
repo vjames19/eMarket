e96d822926fe110fd070a180966379f445301c95
samy100 <tech_boy@outlook.com>
2013-10-07 04:10:15 -0400

    adding add user for admin

--- public/js/directives/addUser.js
+++ public/js/directives/addUser.js
@@ -9,8 +9,31 @@ angular.module('eMarketApp')
         replace: true,
         controller: function ($scope, Restangular) {
           $scope.submit = function () {
-            Restangular.all('api/users').post($scope.addUser);
-            $.mobile.changePage('#user-accounts', {transition: 'fade'});
+            $.mobile.loading('show');
+            Restangular.all('api/users').post($scope.addUser.user).then(function(user) {
+              Restangular.one('api/users', user.userId).all('mailAddresses').post($scope.addUser.mailAddress);
+              Restangular.one('api/users', user.userId).all('billAddresses').post($scope.addUser.billAddress);
+              Restangular.one('api/users', user.userId).all('creditCards').post($scope.addUser.creditCard);
+              $.mobile.loading('hide');
+              $.mobile.changePage('#user-accounts', {transition: 'fade'});
+            });
+          };
+        },
+        link: function (scope, elem) {
+          var page = $(elem[0]);
+          var sameAsCheckBox = page.find('#same-as');
+          var billAddress = page.find('#billing-address');
+          var selectCountryBillingAddress = page.find('#country-billing-address');
+          var selectStateBillingAddress = page.find('#geographical-region-billing-address');
+          var cityBillingAddress = page.find('#city-billing-address');
+          var zipCodeBillingAddress = page.find('#zip-code-billing-address');
+
+          scope.disableBillAddress = function () {
+            billAddress.prop('disabled', sameAsCheckBox.prop('checked'));
+            selectCountryBillingAddress.prop('disabled', sameAsCheckBox.prop('checked'));
+            selectStateBillingAddress.prop('disabled', sameAsCheckBox.prop('checked'));
+            cityBillingAddress.prop('disabled', sameAsCheckBox.prop('checked'));
+            zipCodeBillingAddress.prop('disabled', sameAsCheckBox.prop('checked'));
           };
         }
       };
