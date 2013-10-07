efdd038ca8df7bae4e1c7ee18238a4114e40a8ce
samy100 <tech_boy@outlook.com>
2013-10-06 20:22:49 -0400

    Adding add admin functionality and admin factory

--- public/js/directives/addAdmin.js
+++ public/js/directives/addAdmin.js
@@ -6,7 +6,14 @@ angular.module('eMarketApp')
         templateUrl: 'views/addAdmin.html',
         restrict: 'E',
         scope: {},
-        replace: true
+        replace: true,
+        controller: function ($scope, Restangular) {
+          $scope.submit = function () {
+            console.log($scope.admin);
+            Restangular.all('api/admins').post($scope.admin);
+            $.mobile.changePage('#home-admin', {transition: 'fade'});
+          };
+        }
       };
     });
 
