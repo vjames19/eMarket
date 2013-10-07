5e54e8cfcff422f82a16f17372cc0a58bff3fb69
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-07 09:32:14 -0400

    Removing admin code from user app

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -42,35 +42,14 @@ angular.module('eMarketApp').controller('MainCtrl', function($scope, Auth) {
     $scope.password = '';
   };
 
-  // Admins Controllers
-  $scope.adminInfoToBeViewed = null;
-  $scope.userInfoToBeViewed = null;
-  $scope.categoryInfoToBeViewed = null;
-
-  $scope.setAdminInfo = function(adminInfo) {
-    $scope.adminInfoToBeViewed = angular.copy(adminInfo);
-  };
-
-  $scope.setUserInfo = function(userInfo) {
-    $scope.userInfoToBeViewed = angular.copy(userInfo);
-  };
-
-  $scope.setCategoryInfo = function(categoryInfo) {
-    $scope.categoryInfoToBeViewed = angular.copy(categoryInfo);
-  };
-
-  $scope.submitSearch = function() {
-    $.mobile.changePage('#search-results', {data: $scope.searchQuery});
-  };
-
   $scope.refreshDom = function() {
     $.mobile.changePage(
         window.location.href,
         {
-          allowSamePageTransition : true,
-          transition              : 'none',
-          showLoadMsg             : false,
-          reloadPage              : true
+          allowSamePageTransition: true,
+          transition: 'none',
+          showLoadMsg: false,
+          reloadPage: true
         }
     );
   };
