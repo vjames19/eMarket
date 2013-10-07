2f3a797f81fe946b3ddf31bf26ae12cb5179e379
samy100 <tech_boy@outlook.com>
2013-10-06 21:29:35 -0400

    Adding add category

--- public/js/directives/addCategory.js
+++ public/js/directives/addCategory.js
@@ -1,12 +1,29 @@
 'use strict';
 
 angular.module('eMarketApp')
-    .directive('addCategory', function () {
+    .directive('addCategory', function (Restangular) {
       return {
         templateUrl: 'views/addCategory.html',
         restrict: 'E',
         scope: {},
-        replace: true
+        replace: true,
+        controller: function ($scope, Restangular) {
+          $scope.submit = function () {
+            if(!$scope.addCategory.categoryParent){
+              $scope.addCategory.categoryParent = null;
+            }
+            Restangular.all('api/categories').post($scope.addCategory);
+            $.mobile.changePage('#categories-admin', {transition: 'fade'});
+          };
+        },
+        link: function (scope, elem) {
+          var page = $(elem[0]);
+
+          page.on('pagebeforeshow', function () {
+            scope.categories = Restangular.all('api/categories').getList();
+          });
+
+        }
       };
     });
 
