a20a6be9ff94a1c22299aff3d36cab465e0a328f
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-07 12:00:23 -0400

    Implemented category navigation.

--- public/js/directives/categories.js
+++ public/js/directives/categories.js
@@ -1,24 +1,55 @@
 'use strict';
 
 angular.module('eMarketApp')
-  .directive('categories', function (Category) {
-    return {
-      templateUrl: 'views/categories.html',
-      restrict: 'E',
-      scope: {},
-      replace: true,
-      link: function(scope, elem) {
-        var page = $(elem[0]);
-        var categoryList = page.find('#categoryList');
+    .directive('categories', function(Category) {
+      return {
+        templateUrl: 'views/categories.html',
+        restrict: 'E',
+        scope: {},
+        replace: true,
+        link: function(scope, elem) {
+          var page = $(elem[0]);
+          var upButton = page.find('#up-button');
+          var categoryList = page.find('#categoryList');
+          var stack = [];
 
-        page.on('pagebeforeshow', function() {
-          scope.categories = Category.getList();
-        });
+          var refreshList = function(categories) {
+            scope.categories = categories;
+            setTimeout(function() { // It doesn't work without it -.-
+              categoryList.listview('refresh');
+            }, 1)
+          }
 
-        page.on('pageshow', function() {
-          categoryList.listview('refresh');
-        });
-      }
-    };
-  });
+          scope.next = function(category) {
+            if(angular.isArray(category.categories)) {
+              upButton.show();
+              stack.push(category.categories);
+              refreshList(category.categories);
+            } else {
+              $.mobile.changePage('#search-results', {q: category.categoryName});
+            }
+          };
+
+          scope.prev = function() {
+            stack.pop();
+            if(stack.length <= 1) {
+              upButton.hide();
+            }
+            refreshList(_.last(stack));
+          }
+
+          page.on('pagebeforeshow', function() {
+            upButton.hide();
+            stack = [];
+            scope.categories = Category.getList();
+            stack.push(scope.categories);
+          });
+
+          page.on('pageshow', function() {
+            categoryList.listview('refresh');
+          });
+        }
+
+      };
+    });
 
