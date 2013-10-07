ba5f8ed8fe3a29a5b05f9fdfa2abf9a77139cf09
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-07 16:18:28 -0400

    Remove if that was making the view to behave unexpectedly

--- public/js/directives/itemView.js
+++ public/js/directives/itemView.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('eMarketApp')
-    .directive('itemView', function (User) {
+    .directive('itemView', function(User) {
       return {
         templateUrl: 'views/itemView.html',
         restrict: 'E',
@@ -9,40 +9,37 @@ angular.module('eMarketApp')
           item: '='
         },
         replace: true,
-        link: function (scope, elem) {
+        link: function(scope, elem) {
           var page = $(elem[0]);
           var popupItemAddedToCart = page.find('#popupItemAddedToCart');
           var buyItNowButton = page.find('#buy-it-now-button');
           var bidButton = page.find('#place-bid-button');
           var placeBidInput = page.find('#placeBid');
 
-          if (scope.item.productSellerId) {
-            page.on('pagebeforeshow', function () {
-              if(scope.item && User.userId === scope.item.productSellerId) {
-                buyItNowButton.addClass('ui-disabled');
-                bidButton.addClass('ui-disabled');
-              } else {
-                bidButton.removeClass('ui-disabled');
-                buyItNowButton.removeClass('ui-disabled');
-              }
-            });
-          }
+          page.on('pagebeforeshow', function() {
+            if(User.userId === scope.item.productSellerId) {
+              buyItNowButton.addClass('ui-disabled');
+              bidButton.addClass('ui-disabled');
+            } else {
+              bidButton.removeClass('ui-disabled');
+              buyItNowButton.removeClass('ui-disabled');
+            }
+          });
 
-          scope.placeBid = function () {
+          scope.placeBid = function() {
 
           };
 
-          scope.addToCart = function () {
+          scope.addToCart = function() {
             // Get the item quantity and multiply it by the price to get the total cost
             scope.item.cost = scope.item.quantity * scope.item.productBuyItNowPrice;
             User.me().all('carts').post(scope.item);
           };
 
-          scope.placeMinBid = function () {
+          scope.placeMinBid = function() {
             placeBidInput.attr('value', scope.item.productCurrentBidPrice + 5);
           };
 
         }
       };
-    }
-);
+    });
