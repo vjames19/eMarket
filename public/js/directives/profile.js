32b2f162a0fad2765a0b636754955976119b68c7
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-06 22:07:40 -0400

    Implemented PUT on profile.

--- public/js/directives/profile.js
+++ public/js/directives/profile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('eMarketApp')
-    .directive('profile', function (User) {
+    .directive('profile', function(User, Restangular) {
       return {
         templateUrl: 'views/profile.html',
         restrict: 'E',
@@ -13,9 +13,19 @@ angular.module('eMarketApp')
           var billAddressList = page.find('#billAddressList');
           var ratingList = page.find('#ratingList')
 
+          scope.submitUser = function() {
+            $.mobile.loading("show");
+            scope.user.customPUT(scope.user, scope.user.userId).then(function() {
+              $.mobile.loading("hide");
+            });
+          }
+
           page.on('pagebeforeshow', function() {
             var user = User.me();
-            scope.user = user.get();
+            //            scope.user = user.get();
+            user.get().then(function(user) {
+              scope.user = user
+            });
             scope.mailAddresses = user.getList('mailAddresses');
             scope.billAddresses = user.getList('billAddresses');
             scope.ratings = user.getList('ratings');
