4c63c74e7026725f2b905eb3a958f03bd4850a31
samy100 <tech_boy@outlook.com>
2013-10-07 14:55:24 -0400

    improving profile delete.

--- public/js/directives/profile.js
+++ public/js/directives/profile.js
@@ -1,63 +1,81 @@
 'use strict';
 
 angular.module('eMarketApp')
-    .directive('profile', function(User) {
+    .directive('profile', function (User) {
       return {
         templateUrl: 'views/profile.html',
         restrict: 'E',
         scope: true,
         replace: true,
-        link: function(scope, elem) {
+        link: function (scope, elem) {
           var page = $(elem[0]);
           var mailAddressList = page.find('#mailAddressList');
           var billAddressList = page.find('#billAddressList');
           var ratingList = page.find('#ratingList');
           var selectedMailAddress = null;
+          var selectedMailAddressIndex = null;
           var selectedBillAddress = null;
+          var selectedBillAddressIndex = null;
 
-          scope.submitUser = function() {
+          scope.submitUser = function () {
             $.mobile.loading('show');
-            scope.user.customPUT(scope.user, scope.user.userId).then(function() {
+            scope.user.customPUT(scope.user, scope.user.userId).then(function () {
               $.mobile.loading('hide');
             });
           };
 
-          scope.selectedMailAddress = function(mailAddress) {
+          scope.selectMailAddress = function (mailAddress, index) {
             selectedMailAddress = mailAddress;
-          }
+            selectedMailAddressIndex = index;
+          };
 
-          scope.selectedBillAddress = function(billAddress) {
+          scope.selectBillAddress = function (billAddress, index) {
             selectedBillAddress = billAddress;
-          }
+            selectedBillAddressIndex = index;
+          };
 
-          scope.deleteMailAddress = function() {
+          scope.deleteMailAddress = function () {
             $.mobile.loading('show');
-            User.me().one('mailAddresses', selectedMailAddress.mailAddressId).remove();
-            $.mobile.loading('hide');
-            scope.refreshDom();
-          }
+            User.me().one('mailAddresses', selectedMailAddress.mailAddressId).remove().then(function () {
+              scope.mailAddresses.splice(selectedMailAddressIndex, 1);
+              mailAddressList.listview('refresh');
+              $.mobile.loading('hide');
+            });
+          };
 
-          scope.deleteBillAddress = function() {
+          scope.deleteBillAddress = function () {
             $.mobile.loading('show');
-            User.me().one('BillAddresses', selectedBillAddress.billAddressId).remove();
-            $.mobile.loading('hide');
-            scope.refreshDom();
-          }
+            User.me().one('BillAddresses', selectedBillAddress.billAddressId).remove().then(function () {
+              scope.billAddresses.splice(selectedBillAddressIndex, 1);
+              billAddressList.listview('refresh');
+              $.mobile.loading('hide');
+            });
+          };
 
 
+          page.on('pagebeforeshow', function () {
 
-          page.on('pagebeforeshow', function() {
             var user = User.me();
-            //            scope.user = user.get();
-            user.get().then(function(user) {
+
+            user.get().then(function (user) {
               scope.user = user;
             });
-            scope.mailAddresses = user.getList('mailAddresses');
-            scope.billAddresses = user.getList('billAddresses');
-            scope.ratings = user.getList('ratings');
+
+            user.getList('mailAddresses').then(function (mailAddresses) {
+              scope.mailAddresses = mailAddresses;
+            });
+
+            user.getList('billAddresses').then(function (billAddresses) {
+              scope.billAddresses = billAddresses;
+            });
+
+            user.getList('ratings').then(function (ratings) {
+              scope.ratings = ratings;
+            });
+
           });
 
-          page.on('pageshow', function() {
+          page.on('pageshow', function () {
             mailAddressList.listview('refresh');
             billAddressList.listview('refresh');
             ratingList.listview('refresh');
