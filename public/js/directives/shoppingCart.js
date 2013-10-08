d1b8d4bd56b821cfca17caa6c99cd8daeaa78441
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2013-10-07 20:13:16 -0400

    Fixed the computation of total cost + shipping in cart

--- public/js/directives/shoppingCart.js
+++ public/js/directives/shoppingCart.js
@@ -10,56 +10,52 @@ angular.module('eMarketApp')
         link: function(scope, elem) {
           var page = $(elem[0]);
           var shoppingCartList = page.find('#shoppingCartList');
-
-          var cost = null;
-          var shipping = null;
+          var shoppingTotalTable = page.find('#shoppingTotalTable');
 
           var cartSelected = null;
           var selectedCartIndex = null;
 
+          scope.cost = 0;
+          scope.shipping = 0;
+
           page.on('pagebeforeshow', function() {
-            cost = 0;
-            shipping = 0;
             scope.shoppingCarts = User.me().getList('carts').then(function (carts) {
               scope.shoppingCarts = carts;
               for(var i = 0; i < carts.length; i++) {
-                cost += carts[i].cost;
-                shipping += carts[i].productShippingPrice;
+                scope.cost += carts[i].cost;
+                scope.shipping += carts[i].productShippingPrice;
               }
             });
-
           });
-
-          scope.getTotalCost = function() {
-            return cost;
-          };
-          scope.getTotalShipping = function() {
-            return shipping;
-          };
 
           scope.selectCart = function(cartItem, index) {
             cartSelected = cartItem;
             selectedCartIndex = index;
-          };
+          }
+
+          var computeTotalCostAndShipping = function() {
+            scope.cost = 0;
+            scope.shipping = 0;
+            for(var i = 0; i < scope.shoppingCarts.length; i++) {
+              scope.cost += scope.shoppingCarts[i].cost;
+              scope.shipping += scope.shoppingCarts[i].productShippingPrice;
+            }
+          }
 
           scope.deleteCartItem = function() {
             $.mobile.loading('show');
             User.me().one('carts', cartSelected.cartId).remove().then(function () {
               scope.shoppingCarts.splice(selectedCartIndex, 1);
+              computeTotalCostAndShipping();
               shoppingCartList.listview('refresh');
               $.mobile.loading('hide');
             });
-          };
+          }
+
 
           page.on('pageshow', function() {
-            cost = 0;
-            shipping = 0;
-            for(var i = 0; i < scope.shoppingCarts.length; i++) {
-              cost += scope.shoppingCarts[i].cost;
-              shipping += scope.shoppingCarts[i].productShippingPrice;
-            }
+            console.log('page show cost', scope.cost, scope.shipping);
             shoppingCartList.listview('refresh');
-
           });
         }
       };
