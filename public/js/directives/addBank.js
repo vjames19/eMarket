7a8fcddb70a26fe51d34db0434ad6132ca4a71dc
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-07 23:49:55 -0400

    Fixing 304

--- public/js/directives/addBank.js
+++ public/js/directives/addBank.js
@@ -16,7 +16,9 @@ angular.module('eMarketApp')
         link: function (scope, elem) {
           var page = $(elem[0]);
           page.on('pagebeforeshow', function () {
-            scope.billAddresses = User.me().all('billaddresses').getList();
+            User.me().all('billaddresses').getList().then(function(addresses) {
+              scope.billAddresses = addresses;
+            });
           });
         }
       };
