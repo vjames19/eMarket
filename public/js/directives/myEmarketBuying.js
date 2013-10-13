d490abe320e886d001b5da57c8ab5561f6fb20ef
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-12 20:22:29 -0400

    Simplified the logic to get the product of the selected bid. Also renamed biddings to bids and bids to bid.

--- public/js/directives/myEmarketBuying.js
+++ public/js/directives/myEmarketBuying.js
@@ -12,27 +12,21 @@ angular.module('eMarketApp')
           var bidAndPurchaseList = page.find('#bidAndPurchaseList');
 
           scope.selectedBid = function(selectedBid) {
-            Restangular.all('products').getList().then(function(products) {
-              if(window._.contains(window._.pluck(products, 'productId'), selectedBid.productId)) {
-                Restangular.one('products', selectedBid.productId).get().then(function(item) {
-                  console.log('resolved item', item);
-                  scope.setItem(item);
-                  $.mobile.changePage('#item-view');
-                });
-              }
+            Restangular.one('products', selectedBid.productId).get().then(function(product) {
+              scope.setItem(product);
+              $.mobile.changePage('#item-view');
             });
           };
 
           page.on('pagebeforeshow', function() {
             scope.purchases = User.me().getList('purchases');
-            scope.biddings = User.me().getList('bids');
+            scope.bids = User.me().getList('bids');
           });
 
           page.on('pageshow', function() {
             bidAndPurchaseList.listview('refresh');
           });
         }
-
       };
     });
 
