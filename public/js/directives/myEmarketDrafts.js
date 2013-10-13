2dfe533bad5e03794c1411df561359ac77dcfd08
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-12 20:30:42 -0400

    drafts list refreshing.

--- public/js/directives/myEmarketDrafts.js
+++ public/js/directives/myEmarketDrafts.js
@@ -1,23 +1,22 @@
 'use strict';
 
-angular.module('eMarketApp')
-  .directive('myEmarketDrafts', function (User) {
-    return {
-      templateUrl: 'views/myEmarketDrafts.html',
-      restrict: 'E',
-      scope: {},
-      replace: true,
-      link: function(scope, elem) {
-        var page = $(elem[0]);
-        var draftList = page.find('#draftList');
+angular.module('eMarketApp').directive('myEmarketDrafts', function(User) {
+  return {
+    templateUrl: 'views/myEmarketDrafts.html',
+    restrict: 'E',
+    scope: {},
+    replace: true,
+    link: function(scope, elem) {
+      var page = $(elem[0]);
+      var draftList = page.find('#draftList');
 
-        page.on('pagebeforeshow', function() {
-          scope.drafts = User.me().getList('drafts');
+      page.on('pagebeforeshow', function() {
+        scope.drafts = User.me().getList('drafts').then(function() {
+          setTimeout(function() {
+            draftList.listview('refresh');
+          });
         });
-
-        page.on('pageshow', function() {
-          draftList.listview('refresh');
-        });
-      }
-    };
-  });
+      });
+    }
+  };
+});
