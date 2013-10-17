6f590f27e0ef85fd1ab51f2cd8e36f0f84824900
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-17 13:12:00 -0400

    Sanity check in the mapper.

--- app/mapper.js
+++ app/mapper.js
@@ -1,13 +1,15 @@
 'use strict';
 
 module.exports.map = function(object, dictionary) {
-  var obj = {};
-  Object.keys(dictionary).forEach(function(key) {
-    if(object.hasOwnProperty(key)) {
-      var renamedKey = dictionary[key];
-      obj[renamedKey] = object[key];
-    }
-  });
 
+  var obj = {};
+  if(object && dictionary) {
+    Object.keys(dictionary).forEach(function(key) {
+      if(object.hasOwnProperty(key)) {
+        var renamedKey = dictionary[key];
+        obj[renamedKey] = object[key];
+      }
+    });
+  }
   return obj;
 };
