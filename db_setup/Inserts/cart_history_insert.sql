e759f5d49aa9b1bdd3a72ec484c8a7bfb40a0f2d
samy100 <samuel.matos@upr.edu>
2013-10-18 17:10:30 -0400

    adding cart sql templates

---
+++ db_setup/Inserts/cart_history_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `emarket_test`.`cart_history`
+(`cart_id`, `cart_user_id`, `cart_closed_date`)
+VALUES
+(),
---
+++ db_setup/Inserts/cart_item_history_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `emarket_test`.`cart_item_history`
+(`cart_item_cart_id`, `cart_item_product_id`, `cart_item_quantity`, `cart_item_creation_date`, `cart_item_closed_date`)
+VALUES
+(),
