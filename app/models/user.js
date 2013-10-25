445d81da32a2a7ccda25b3a72cdc48461b69e2d0
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-25 18:33:56 -0400

    User model.

---
+++ app/models/user.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var _ = require('underscore');
+var mapper = require('../mapper');
+
+var DICTIONARY = {
+  'user_id': 'id',
+  'user_first_name': 'firstName',
+  'user_middle_name': 'middleName',
+  'user_last_name': 'lastName',
+  'user_telephone': 'telephone',
+  'user_creation_date': 'creationDate'
+};
+
+//var WHITELIST = [];
+
+var executor = null;
+module.exports.init = function(realExecutor) {
+  executor = realExecutor;
+  return module.exports;
+};
+
+module.exports.getAll = function(callback) {
+  executor.execute(function(err, connection) {
+    var sql = 'SELECT user_info.* FROM user_info INNER JOIN user_account_status ' +
+        'ON user_info.user_id = user_account_status.user_account_id ' +
+        'WHERE user_account_status=1';
+    connection.query(sql, function(err, users) {
+      if(err) {
+        callback(err);
+      } else {
+        users = _.map(users, function(user) {
+          return mapper.map(user, DICTIONARY);
+        });
+        callback(null, users);
+      }
+    });
+  });
+};
+
+module.exports.get = function(id, callback) {
+  executor.execute(function(err, connection) {
+    var sql = 'SELECT user_info.* FROM user_info INNER JOIN user_account_status ' +
+        'ON user_info.user_id = user_account_status.user_account_id ' +
+        'WHERE user_id = ? AND user_account_status=1';
+    connection.query(sql, [id], function(err, users) {
+      callback(err, mapper.map(users[0], DICTIONARY));
+    });
+  });
+};
+
+module.exports.authenticate = function(username, password, callback) {
+  executor.execute(function(err, connection) {
+    var sql = 'SELECT user_login_user_name ' +
+        'FROM user_login_info INNER JOIN user_account_status ON user_account_id = user_login_id ' +
+        'WHERE user_login_user_name = ? AND user_login_password = SHA1(?) AND user_account_status=1';
+    connection.query(sql, [username, password], function(err, users) {
+      callback(err, users.length > 0);
+    });
+  });
+};
