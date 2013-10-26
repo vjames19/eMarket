a2dbc96370eb915d52776cbac7aba94ab135488a
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-25 20:06:17 -0400

    Added the userId to the query.

--- app/models/invoice.js
+++ app/models/invoice.js
@@ -17,14 +17,15 @@ module.exports.init = function(realExecutor) {
   return module.exports;
 };
 
-module.exports.getAll = function(callback) {
+module.exports.getAll = function(userId, callback) {
   executor.execute(function(err, connection) {
     var sql = 'SELECT invoice_id, sum(invoice_item_quantity * invoice_item_sold_price) as total, invoice_creation_date ' +
-    'FROM invoice_history inner join user_info inner join invoice_item_history ' +
-    'ON (invoice_history.invoice_id=user_info.user_id AND invoice_id=invoice_item_id) ' +
-    'GROUP BY invoice_id, invoice_creation_date ' +
-    'ORDER BY invoice_creation_date desc';
-    connection.query(sql, function(err, invoices) {
+        'FROM invoice_history inner join user_info inner join invoice_item_history ' +
+        'ON (invoice_history.invoice_id=user_info.user_id AND invoice_id=invoice_item_id) ' +
+        'WHERE user_id = ?' +
+        'GROUP BY invoice_id, invoice_creation_date ' +
+        'ORDER BY invoice_creation_date desc';
+    connection.query(sql, [userId], function(err, invoices) {
       if(err) {
         callback(err);
       } else {
