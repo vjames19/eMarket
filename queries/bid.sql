ebafa0a979d0cda53c0ea214bc7ab0e9fe69946b
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-30 11:08:57 -0400

    More queries.

---
+++ queries/bid.sql
@@ -0,0 +1,1 @@
+SELECT * FROM bid_history WHERE bid_user_id = ? ORDER BY bid_creation_date DESC
---
+++ queries/billingAddress.sql
@@ -0,0 +1,6 @@
+SELECT billing_id, billing_recipient_name, billing_telephone,
+address_address, address_country, address_city, address_geographical_region, address_zipcode
+FROM billing_info INNER JOIN address_history INNER JOIN user_info
+ON (billing_info.billing_address_id=address_history.address_id) AND (billing_info.billing_user_id=user_info.user_id)
+WHERE user_info.user_id = ? AND billing_info.billing_status = 1 AND billing_info.billing_id = ?
+ORDER BY address_geographical_region;
