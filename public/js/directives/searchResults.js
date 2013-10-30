32a20f09fc5426f53526431e39f5ca2ab89d6e38
Victor J. Reventos <victor.reventos1@gmail.com>
2013-10-29 20:32:24 -0400

    Connected the search on the front end.

--- public/js/directives/searchResults.js
+++ public/js/directives/searchResults.js
@@ -1,22 +1,26 @@
 'use strict';
 
-angular.module('eMarketApp').directive('searchResults', function(Restangular) {
+angular.module('eMarketApp').directive('searchResults', function(Restangular, $http) {
   return {
     templateUrl: 'views/searchResults.html',
     restrict: 'E',
-    scope: true,
+    scope: {
+      searchQuery: '='
+    },
     replace: true,
     link: function(scope, elem) {
       var page = $(elem[0]);
       var resultList = page.find('#resultList');
 
-      page.on('pagebeforeshow', function(event, searchQuery) {
-        //            scope.results = Restangular.customGETLIST('search', {q: searchQuery});
-        Restangular.all('products').getList().then(function(results) {
+      page.on('pagebeforeshow', function(event, data) {
+        var searchInput = data.prevPage.find("#search");
+        var searchQuery = searchInput.val();
+        $http.get('api/search', {params: {q: searchQuery}}).success(function(results) {
           scope.results = results;
           setTimeout(function() {
             resultList.listview('refresh');
           });
+          searchInput.val("");
         });
       });
     }
