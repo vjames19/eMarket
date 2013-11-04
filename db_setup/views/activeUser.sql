b32f520cf1bf22ba82d058bc286202eba386cc4e
samy100 <samuel.matos@upr.edu>
2013-11-03 21:04:38 -0400

    updating some queries

--- db_setup/views/activeUser.sql
+++ db_setup/views/activeUser.sql
@@ -1,5 +1,5 @@
 CREATE VIEW active_users AS
 SELECT user_info.*, user_login_user_name, user_login_email
 FROM user_info INNER JOIN user_account_status INNER JOIN user_login_info
-ON (user_info.user_id=user_account_status.user_account_id AND user_id=user_login_id)
-WHERE user_account_status=1
+ON (user_info.user_id = user_account_status.user_account_id AND user_id = user_login_id)
+WHERE user_account_status = 1;
