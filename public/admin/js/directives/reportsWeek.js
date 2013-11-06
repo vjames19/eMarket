d6f8c9996170687fc8bef5f9912f2b60fd09bce0
samy100 <samuel.matos@upr.edu>
2013-11-06 04:16:55 -0400

    Finished report model, reload of graph still doesn't work live, changing pages reflects the new data.. Added new UI to admin.

--- public/admin/js/directives/reportsWeek.js
+++ public/admin/js/directives/reportsWeek.js
@@ -9,28 +9,49 @@ angular.module('eMarketApp')
         replace: true,
         link: function(scope, elem) {
           var page = $(elem[0]);
-          var graph = page.find('#pie');
+          var graph = page.find('#graph');
+          var selectedCategory = page.find('#select-category');
           var totalRevenue = 0.0;
           var totalSales = 0.0;
-          scope.reports = {};
-          scope.reports.totals = {};
+          scope.selectedCategory = null;
+
+          scope.$watch('selectedCategory', function() {
+
+            scope.reportTotals = {sales: 0.0, revenue: 0.0};
+
+            if(scope.selectedCategory === null) {
+              Restangular.one('reportsWeekTotal').get().then(function(result) {
+                scope.reportTotals = result[0];
+                if(!isNaN(scope.reportTotals.sales) && !isNaN(scope.reportTotals.revenue)) {
+                  totalSales = scope.reportTotals.sales;
+                  totalRevenue = scope.reportTotals.revenue;
+                  graph.series[0].setData([totalSales]);
+                  graph.series[1].setData([totalRevenue]);
+                  graph.redraw();
+                }
+              });
+            } else {
+              Restangular.one('reportsWeek', [scope.selectedCategory]).get().then(function(result) {
+                scope.reportTotals = result;
+                if(!isNaN(scope.reportTotals.sales) && !isNaN(scope.reportTotals.revenue)) {
+                  totalSales = scope.reportTotals.sales;
+                  totalRevenue = scope.reportTotals.revenue;
+                  graph.series[0].setData([totalSales]);
+                  graph.series[1].setData([totalRevenue]);
+                  graph.redraw();
+                }
+              });
+            }
+
+          });
 
           page.on('pagebeforeshow', function() {
 
             Restangular.all('categories').getList({flat: true}).then(function(categories) {
               scope.categories = categories;
-            });
-            Restangular.all('reportsWeek').getList().then(function(results) {
-//              console.log('reports: ', JSON.stringify(results));
-              scope.reports = results;
-            });
-            Restangular.one('reportsWeekTotal').get().then(function(result) {
-//              console.log('totals: ', JSON.stringify(result));
-              scope.reportTotals = result[0];
-              if(scope.reportTotals.sales >= 0 && scope.reportTotals.revenue >= 0) {
-                totalSales = scope.reportTotals.sales;
-                totalRevenue = scope.reportTotals.revenue;
-              }
+              setTimeout(function() {
+                selectedCategory.selectmenu('refresh', true);
+              });
             });
 
           });
@@ -38,6 +59,7 @@ angular.module('eMarketApp')
           page.on('pageshow', function() {
             graph.highcharts({
               chart: {
+                type: 'column',
                 plotBackgroundColor: null,
                 plotBorderWidth: null,
                 plotShadow: false,
@@ -47,36 +69,89 @@ angular.module('eMarketApp')
               title: {
                 text: 'Sales and Revenue'
               },
-              tooltip: {
-                enabled: false
+              xAxis: {
+                categories: ['Sales and Revenue']
               },
-              plotOptions: {
-                pie: {
-                  allowPointSelect: true,
-                  dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b><br/>${y:.1f}'
+              yAxis: {
+                title: {
+                  text: 'Amount (USD)'
+                }
+              },
+              tooltip: {
+                formatter: function() {
+                  var number = Highcharts.numberFormat(this.y, 2);
+                  if(this.y >= 0) {
+                    return '<b>' + this.series.name + '</b>: $' + number;
+                  } else {
+                    number = number.replace('-', '-$');
+                    return '<b>' + this.series.name + '</b>: ' + number;
                   }
                 }
               },
-              series: [
-                {
-                  type: 'pie',
-                  name: 'Result',
-                  data: [
-                    ['Sales', totalSales],
-                    ['Revenue', totalRevenue]
-                  ]
-                }
-              ],
               exporting: {
                 enabled: false
               },
               credits: {
                 enabled: false
-              }
+              },
+              series: [
+                {
+                  name: 'Sales',
+                  data: [totalSales]
+                },
+                {
+                  name: 'Revenue',
+                  data: [totalRevenue]
+                }
+              ]
             });
           });
+
+//          page.on('pageshow', function() {
+//            graph.highcharts({
+//              chart: {
+//                plotBackgroundColor: null,
+//                plotBorderWidth: null,
+//                plotShadow: false,
+//                width: $.mobile.window.innerWidth() - 30, ///300,
+//                height: $.mobile.window.innerHeight() / 1.75 //250
+//              },
+//              title: {
+//                text: 'Sales and Revenue'
+//              },
+//              tooltip: {
+//                enabled: false
+//              },
+//              plotOptions: {
+//                pie: {
+//                  allowPointSelect: true,
+//                  dataLabels: {
+//                    enabled: true,
+//                    format: '<b>{point.name}</b><br/>${y:.1f}'
+//                  }
+//                }
+//              },
+//              series: [
+//                {
+//                  type: 'pie',
+//                  name: 'Result',
+//                  data: [
+//                    ['Sales', totalSales],
+//                    ['Revenue', totalRevenue]
+//                  ]
+//                }
+//              ],
+//              exporting: {
+//                enabled: false
+//              },
+//              credits: {
+//                enabled: false
+//              }
+//            });
+//          });
+
+          scope.$digest();
+
         }
       };
     });
