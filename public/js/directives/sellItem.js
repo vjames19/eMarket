865a53d961862b8388598165ed6e83dac74fdd44
samy100 <samuel.matos@upr.edu>
2013-11-06 22:14:33 -0400

    Adding some checks and validation for automatic selection of checkboxes and other things when its a draft in sellItem.

--- public/js/directives/sellItem.js
+++ public/js/directives/sellItem.js
@@ -9,14 +9,20 @@ angular.module('eMarketApp').directive('sellItem', function(Category, SellItem) 
     },
     replace: true,
     link: function(scope, elem) {
+
       var page = $(elem[0]);
       var freeShippingCheckbox = page.find('#checkbox-free-shipping');
       var shippingPriceInput = page.find('#shipping-price');
+
+      var newCheckBox = page.find('#radio-choice-new');
+      var UsedCheckBox = page.find('#radio-choice-used');
+      var RefurbishedCheckBox = page.find('#radio-choice-refurbished');
 
       page.on('pagebeforeshow', function() {
 
         scope.disableShipping = function() {
           shippingPriceInput.prop('disabled', freeShippingCheckbox.prop('checked'));
+          scope.item.shippingPrice = 0;
         };
 
         Category.getList({flat: true}).then(function(categoryList) {
@@ -30,6 +36,42 @@ angular.module('eMarketApp').directive('sellItem', function(Category, SellItem) 
         scope.setPreviewItemInfo = function(item) {
           SellItem.itemPreview = item;
         };
+
+        if(SellItem.isDraft && scope.item.condition === 'New') {
+          setTimeout(function() {
+            newCheckBox.prop('checked', true).checkboxradio('refresh');
+            UsedCheckBox.prop('checked', false).checkboxradio('refresh');
+            RefurbishedCheckBox.prop('checked', false).checkboxradio('refresh');
+          });
+        } else if(SellItem.isDraft && scope.item.condition === 'Used') {
+          setTimeout(function() {
+            newCheckBox.prop('checked', false).checkboxradio('refresh');
+            UsedCheckBox.prop('checked', true).checkboxradio('refresh');
+            RefurbishedCheckBox.prop('checked', false).checkboxradio('refresh');
+          });
+        } else if(SellItem.isDraft && scope.item.condition === 'Refurbished') {
+          setTimeout(function() {
+            newCheckBox.prop('checked', false).checkboxradio('refresh');
+            UsedCheckBox.prop('checked', false).checkboxradio('refresh');
+            RefurbishedCheckBox.prop('checked', true).checkboxradio('refresh');
+          });
+        } else {
+          setTimeout(function() {
+            newCheckBox.prop('checked', false).checkboxradio('refresh');
+            UsedCheckBox.prop('checked', false).checkboxradio('refresh');
+            RefurbishedCheckBox.prop('checked', false).checkboxradio('refresh');
+          });
+        }
+
+        if(SellItem.isDraft && scope.item.shippingPrice === 0) {
+          setTimeout(function() {
+            freeShippingCheckbox.prop('checked', true).checkboxradio('refresh');
+          });
+        } else {
+          setTimeout(function() {
+            freeShippingCheckbox.prop('checked', false).checkboxradio('refresh');
+          });
+        }
 
       });
 
