a632686ef070c51a188d23043c57bd86f26f5faa
samy100 <samuel.matos@upr.edu>
2013-11-11 19:12:49 -0400

    Cleaning Seller Profile

--- public/js/directives/sellerProfile.js
+++ public/js/directives/sellerProfile.js
@@ -1,30 +1,34 @@
 'use strict';
 
-angular.module('eMarketApp').directive('sellerProfile', function(Restangular, SellerInfo) {
+angular.module('eMarketApp').directive('sellerProfile', function(Restangular, SellerInfo, Helper) {
   return {
     templateUrl: 'views/sellerProfile.html',
     restrict: 'E',
-    scope: true,
+    scope: {},
     replace: true,
     controller: function($scope, Product) {
+
       $scope.setItem = Product.setItem;
+
     },
     link: function(scope, elem) {
+
       var page = $(elem[0]);
       var sellerProductList = page.find('#sellerProductList');
       var sellerRatingList = page.find('#sellerRatingList');
 
       page.on('pagebeforeshow', function() {
+
         Restangular.one('users', SellerInfo.sellerId).getList('unsoldProducts').then(function(products) {
           scope.products = products;
-          setTimeout(function() {
-            sellerProductList.listview('refresh');
-          });
+          Helper.refreshList(sellerProductList);
         });
+
         scope.sellerName = SellerInfo.sellerName;
         scope.sellerAvgRate = SellerInfo.sellerAvgRate;
 
         setTimeout(function() {
+
           page.find('#sellerAvgRate').raty({
             cancel: true,
             score: function() {
@@ -35,19 +39,23 @@ angular.module('eMarketApp').directive('sellerProfile', function(Restangular, Se
             readOnly: true,
             path: '../lib/raty/lib/img'
           });
-          sellerRatingList.listview('refresh');
+
+          page.find('#rateSeller').raty({
+            cancel: true,
+            score: function() {
+              return $(this).attr('data-score');
+            },
+            half: true,
+            size: 16,
+            path: '../lib/raty/lib/img'
+          });
+
+          Helper.refreshList(sellerRatingList);
+
         });
+
       });
 
-      page.find('#rateSeller').raty({
-        cancel: true,
-        score: function() {
-          return $(this).attr('data-score');
-        },
-        half: true,
-        size: 16,
-        path: '../lib/raty/lib/img'
-      });
     }
   };
 });
