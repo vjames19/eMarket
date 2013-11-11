698ad4853fb68980c387d8de928a1d7be7f21d54
samy100 <samuel.matos@upr.edu>
2013-11-11 13:42:26 -0400

    Code clean up disabled post/put/del requests so the application behaves perfectly fine even if they are not implemented.

--- public/js/directives/forgotPassword.js
+++ public/js/directives/forgotPassword.js
@@ -1,19 +1,34 @@
 'use strict';
 
-angular.module('eMarketApp').directive('forgotPassword', function(Restangular) {
+angular.module('eMarketApp').directive('forgotPassword', function(Restangular, Helper) {
   return {
     templateUrl: 'views/forgotPassword.html',
     restrict: 'E',
     scope: {},
     replace: true,
+    controller: function($scope) {
+
+      $scope.submit = function() {
+        $.mobile.loading('show');
+        // Restangular call goes here.
+        $.mobile.loading('hide');
+        $.mobile.changePage('#index-page'); // <- TODO: open a popup with his password
+      };
+
+    },
     link: function(scope, elem) {
+
       var page = $(elem[0]);
+
+      var securityQuestions = page.find('select[id*="security-questions"]');
 
       page.on('pagebeforeshow', function() {
 
         Restangular.one('questions').getList().then(function(questionsList) {
           scope.questions = questionsList;
+          Helper.refreshSelect(securityQuestions);
         });
+
       });
 
     }
--- public/js/directives/register.js
+++ public/js/directives/register.js
@@ -1,45 +1,48 @@
 'use strict';
 
-angular.module('eMarketApp').directive('register', function(Restangular) {
+angular.module('eMarketApp').directive('register', function(Restangular, Helper) {
   return {
     templateUrl: 'views/register.html',
     restrict: 'E',
     scope: {},
     replace: true,
     controller: function($scope, Restangular) {
+
       $scope.newUser = { creditCard: {creditCardType: 'Visa'} };
 
       $scope.submit = function() {
         $.mobile.loading('show');
-        Restangular.all('users').post($scope.newUser.user).then(function(user) {
-          user.all('mailAddresses').post($scope.newUser.mailAddress);
-          user.all('billAddresses').post($scope.newUser.billAddress);
-          user.all('creditCards').post($scope.newUser.creditCard);
-          $.mobile.loading('hide');
-          $.mobile.changePage('#index-user'); //Force user to put hes newly registered credentials
-        });
+//        Restangular.all('users').post($scope.newUser.user).then(function(user) {
+//          user.all('mailAddresses').post($scope.newUser.mailAddress);
+//          user.all('billAddresses').post($scope.newUser.billAddress);
+//          user.all('creditCards').post($scope.newUser.creditCard);
+        $.mobile.loading('hide');
+        $.mobile.changePage('#index-page'); //Force user to put hes newly registered credentials
+//        });
       };
 
     },
     link: function(scope, elem) {
+
       var page = $(elem[0]);
       var sameAsCheckBox = page.find('#same-as');
+      var securityQuestions = page.find('select[id*="security-questions"]');
+      var billingAddressFields = page.find('input[id*="billing-address"]');
 
       scope.disableBillAddress = function() {
         var disable = sameAsCheckBox.prop('checked');
-        page.find('input[id*="billing-address"]').prop('disabled', disable);
+        billingAddressFields.prop('disabled', disable);
       };
 
       page.on('pagebeforeshow', function() {
 
         Restangular.one('questions').getList().then(function(questionsList) {
           scope.questions = questionsList;
-          setTimeout(function() {
-            page.find('select[id*="security-questions"]').selectmenu('refresh', true);
-          });
+          Helper.refreshSelect(securityQuestions);
         });
 
       });
+
     }
   };
 });
