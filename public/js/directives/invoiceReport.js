5943b1fb668b10b9b339b32d0ef77c48f54e3b45
samy100 <samuel.matos@upr.edu>
2013-11-11 16:41:42 -0400

    Code Clean

--- public/js/directives/invoiceReport.js
+++ public/js/directives/invoiceReport.js
@@ -1,24 +1,26 @@
 'use strict';
 
-angular.module('eMarketApp').directive('invoiceReport', function(User) {
+angular.module('eMarketApp').directive('invoiceReport', function(User, Helper) {
   return {
     templateUrl: 'views/invoiceReport.html',
     restrict: 'E',
     scope: {},
     replace: true,
     link: function(scope, elem) {
+
       var page = $(elem[0]);
       var invoiceProductsList = page.find('#invoiceProductsList');
 
       page.on('pagebeforeshow', function() {
+
         var invoiceId = User.userId;
         User.me().one('invoices', invoiceId).getList('products').then(function(invoiceProducts) {
           scope.invoiceProducts = invoiceProducts;
-          setTimeout(function() {
-            invoiceProductsList.listview('refresh');
-          });
+          Helper.refreshList(invoiceProductsList);
         });
+
       });
+
     }
   };
 });
--- public/js/directives/invoices.js
+++ public/js/directives/invoices.js
@@ -1,22 +1,23 @@
 'use strict';
 
-angular.module('eMarketApp').directive('invoices', function(User) {
+angular.module('eMarketApp').directive('invoices', function(User, Helper) {
   return {
     templateUrl: 'views/invoices.html',
     restrict: 'E',
     scope: {},
     replace: true,
     link: function(scope, elem) {
+
       var page = $(elem[0]);
       var invoiceList = page.find('#invoiceList');
 
       page.on('pagebeforeshow', function() {
+
         User.me().getList('invoices').then(function(invoices) {
           scope.invoices = invoices;
-          setTimeout(function() {
-            invoiceList.listview('refresh');
-          });
+          Helper.refreshList(invoiceList);
         });
+
       });
 
     }
