d1425d6f41ab2482e5541fa71072740a5e2fab48
samy100 <samuel.matos@upr.edu>
2013-11-11 21:26:05 -0400

    Properly Setting Up Carousel. To change data each time you enter and make it compatible with ng-repeat transformations and plugin transformations.

--- public/js/directives/homeUser.js
+++ public/js/directives/homeUser.js
@@ -7,6 +7,7 @@ angular.module('eMarketApp').directive('homeUser', function(User, Carousel, Help
     scope: {},
     replace: true,
     controller: function($scope, Auth, SellItem, Product) {
+
       $scope.logOut = Auth.logOut;
 
       $scope.setItem = Product.setItem;
@@ -14,25 +15,33 @@ angular.module('eMarketApp').directive('homeUser', function(User, Carousel, Help
       $scope.isDraft = function(isDraft) {
         SellItem.isDraft = angular.copy(isDraft);
       };
+
     },
     link: function(scope, elem) {
 
       var page = $(elem[0]);
       var carousel = page.find('#owl-example');
 
-      page.on('pagebeforecreate', function() {
+      page.on('pagebeforeshow', function() {
 
         User.me().getList('carousels').then(function(products) {
           scope.carouselProducts = products;
           Helper.triggerCreate(carousel);
           setTimeout(function() {
-            $(document).ready(function() {
-              carousel.owlCarousel(Carousel.options);
-            });
+            carousel.owlCarousel(Carousel.options);
           });
         });
 
       });
+
+      page.on('pagehide', function() {
+
+        // unwrap whole plugin and leave original pre carousel structure
+        // need this to reload ng-repeat on the new data DON'T DELETE.
+        carousel.data('owlCarousel').destroy();
+
+      });
+
     }
   };
 });
