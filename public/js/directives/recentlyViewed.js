92f2ee885b96b30e8c60054e9cf6b6cda57cd9d6
samy100 <samuel.matos@upr.edu>
2013-11-11 20:00:41 -0400

    Cleaning and Refactoring

--- public/js/directives/recentlyViewed.js
+++ public/js/directives/recentlyViewed.js
@@ -1,26 +1,30 @@
 'use strict';
 
-angular.module('eMarketApp').directive('recentlyViewed', function(User) {
+angular.module('eMarketApp').directive('recentlyViewed', function(User, Helper) {
   return {
     templateUrl: 'views/recentlyViewed.html',
     restrict: 'E',
-    scope: true,
+    scope: {},
     replace: true,
     controller: function($scope, Product) {
+
       $scope.setItem = Product.setItem;
+
     },
     link: function(scope, elem) {
+
       var page = $(elem[0]);
       var recentlyViewedList = page.find('#recentlyViewedList');
 
       page.on('pagebeforeshow', function() {
+
         User.me().getList('browsedItems').then(function(items) {
           scope.recentlyViewed = items;
-          setTimeout(function() {
-            recentlyViewedList.listview('refresh');
-          });
+          Helper.refreshList(recentlyViewedList);
         });
+
       });
+
     }
   };
 });
