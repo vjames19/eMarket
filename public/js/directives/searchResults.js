829fd82de0225b391aa49bc3a9347900b709bc58
samy100 <samuel.matos@upr.edu>
2013-11-11 20:14:22 -0400

    Cleaning and refactoring searchResults

--- public/js/directives/searchResults.js
+++ public/js/directives/searchResults.js
@@ -1,12 +1,13 @@
 'use strict';
 
-angular.module('eMarketApp').directive('searchResults', function(Restangular, $http, Search) {
+angular.module('eMarketApp').directive('searchResults', function(Restangular, $http, Search, Helper) {
   return {
     templateUrl: 'views/searchResults.html',
     restrict: 'E',
-    scope: true,
+    scope: {},
     replace: true,
     controller: function($scope, Product) {
+
       $scope.setItem = Product.setItem;
       $scope.selectedSortOption = null;
       $scope.sortOptions = [
@@ -18,27 +19,33 @@ angular.module('eMarketApp').directive('searchResults', function(Restangular, $h
       $scope.sortBy = function(sortOption) {
         $scope.selectedSortOption = sortOption;
       };
+
     },
-    link: function($scope, elem) {
+    link: function(scope, elem) {
+
       var page = $(elem[0]);
       var resultList = page.find('#resultList');
+
       var search = function() {
+        $.mobile.loading('show');
         $http.get('api/search', {params: Search.getSearchParams()}).success(function(results) {
-          $scope.results = results;
-          setTimeout(function() {
-            resultList.listview('refresh');
-          });
+          scope.results = results;
+          Helper.refreshList(resultList);
+          $.mobile.loading('hide');
         });
       };
 
-      $scope.submitSearch = function() {
+      scope.submitSearch = function() {
         Search.setSearchQuery(page.find('#search').val());
         search();
       };
 
       page.on('pagebeforeshow', function() {
+
         search();
+
       });
+
     }
   };
 });
