c4abd5e0494c56e443315280b8c98f388aeaad26
samy100 <samuel.matos@upr.edu>
2013-11-30 18:07:37 -0400

    Moving Picture Logic to its own controller.

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var crypto = require('crypto');
-
 module.exports = function(app, passport, auth) {
   // TODO: Create routes and its respectives controllers.
   // User Routes
@@ -21,36 +17,9 @@ module.exports = function(app, passport, auth) {
     res.jsonp(200);
   });
 
-  app.post('/pictures', function(req, res) {
-
-    var fileExt, filePath;
-    var newFileName, newFilePath;
-    var shasum = crypto.createHash('sha1');
-
-    filePath = path.normalize(req.files.file.path);
-    fileExt = path.extname(filePath);
-
-    fs.readFile(filePath, function(err, data) {
-
-      // Using photo data for uniqueness, this way if two users upload same picture
-      // picture will be re-used. TODO <-- is this good? or should we use a random seed?
-      // prev TODO : For example shasum.update(crypto.randomBytes(20))?
-      newFileName = shasum.update(data).digest('hex') + fileExt.toLowerCase();
-      newFilePath = path.join(__dirname, '../public/pictures/' + newFileName);
-
-      console.log('New File Added to: ', newFilePath);
-
-      fs.writeFile(newFilePath, data, function(err) {
-        if(err) {
-          console.log('err', err);
-          res.jsonp(409);
-        } else {
-          res.jsonp(201, newFileName);
-        }
-      });
-    });
-
-  });
+  // Picture
+  var pictureCtrl = require('../app/controllers/picture');
+  app.post('/pictures', pictureCtrl.createPicture);
 
   // TODO(vjames19): Secure users api
 
