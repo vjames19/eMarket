18afc068a7e8c0e4d335b8d7c4287c19f0972474
samy100 <samuel.matos@upr.edu>
2013-12-04 12:55:05 -0400

    Removing Auth for debug mode

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -7,8 +7,8 @@ angular.module('eMarketApp').controller('MainCtrl', function($scope, Auth, Patte
 
   $scope.logIn = function() {
     // TODO: Uncomment for real auth!!!!!
-    Auth.logIn({username: $scope.username, password: $scope.password});
-//    Auth.logIn({username: 'juanba', password: 'juanba_pwd'});
+//    Auth.logIn({username: $scope.username, password: $scope.password});
+    Auth.logIn({username: 'juanba', password: 'juanba_pwd'});
     $scope.username = '';
     $scope.password = '';
   };
