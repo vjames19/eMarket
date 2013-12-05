2189096717b9c2fe3dec90f40b6d0e4bd237f505
samy100 <samuel.matos@upr.edu>
2013-12-05 15:12:03 -0400

    Nice message when no results are found.

--- public/js/directives/searchResults.js
+++ public/js/directives/searchResults.js
@@ -26,23 +26,59 @@ angular.module('eMarketApp').directive('searchResults', function(Restangular, $h
       var page = $(elem[0]);
       var resultList = page.find('#searchResult-resultList');
 
-      var search = function() {
+      var statusPopup = page.find('#searchResult-statusPopup');
+      var statusPopupMessage = page.find('#searchResult-statusPopupMessage');
+
+      var search = function(done) {
         $.mobile.loading('show');
+        scope.results = {};
+        done(false);
         $http.get('api/search', {params: Search.getSearchParams()}).success(function(results) {
           scope.results = results;
           Helper.refreshList(resultList);
           $.mobile.loading('hide');
+          done(true);
         });
       };
 
       scope.submitSearch = function() {
+
         Search.setSearchQuery(page.find('#searchResults-search').val());
-        search();
+
+        // Here for future searches.
+        search(function(done) {
+
+          if(done) {
+
+            if(scope.results.length === 0) {
+              statusPopupMessage.text('No Results Found, Please Try Again.');
+              statusPopup.popup('open');
+            }
+
+          }
+
+        });
+
       };
 
       page.on('pagebeforeshow', function() {
 
-        search();
+        search(function() {
+
+        });
+
+      });
+
+      page.on('pageshow', function() {
+
+        // Here for first search.
+        setTimeout(function() {
+          console.log(scope.results);
+          if(scope.results.length === 0) {
+            statusPopupMessage.text('No Results Found, Please Try Again.');
+            statusPopup.popup('open');
+          }
+        });
 
       });
 
