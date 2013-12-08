673589df1eb7e5697d8a083da5ddfd4aff7e94dd
samy100 <samuel.matos@upr.edu>
2013-12-07 22:25:43 -0400

    missing search error handling

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,15 +1,24 @@
 'use strict';
 
+//var _ = require('underscore');
 var Product = require('../models/product.js');
 
 exports.searchProduct = function(req, res, next) {
   if(req.query.q) {
     Product.search(req.query.q, function(err, products) {
-      res.jsonp(200, products);
+      if(err) {
+        next({code: 500, message: err});
+      } else {
+        res.jsonp(200, products);
+      }
     });
   } else if(req.query.c) {
     Product.searchByCategory(req.query.c, function(err, results) {
-      res.jsonp(200, results);
+      if(err) {
+        next({code: 500, message: err});
+      } else {
+        res.jsonp(200, results);
+      }
     });
   } else {
     next({code: 400, message: 'Need a query parameter.'});
