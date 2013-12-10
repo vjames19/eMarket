394c3d69639b23861a9eb598e0a8510c30ddaff3
samy100 <samuel.matos@upr.edu>
2013-12-10 16:25:14 -0400

    Fix in product Bids.

--- app/models/productBids.js
+++ app/models/productBids.js
@@ -6,9 +6,13 @@ var logger = require('../logger');
 
 var DICTIONARY = {
   'bid_id': 'id',
+  'bid_user_id': 'bidderId',
+  'bid_product_id': 'productId',
   'bid_amount': 'bidAmount',
   'bid_creation_date': 'creationDate',
-  'seller_name': 'seller'
+  'bid_closed_date': 'closedDate',
+  'seller_name': 'sellerName',
+  'user_login_user_name': 'bidderName'
 };
 
 //var WHITELIST = [];
@@ -24,10 +28,9 @@ module.exports.getAll = function(productId, callback) {
     if(err) {
       callback(err);
     } else {
-      var sql = 'SELECT bid_history.bid_id, bid_history.bid_amount, bid_history.bid_creation_date, ' +
-          'products.seller_name ' +
-          'FROM bid_history INNER JOIN products ' +
-          'ON (bid_history.bid_product_id = products.product_id) ' +
+      var sql = 'SELECT bid_history.*,  products.seller_name, active_users.user_login_user_name ' +
+          'FROM bid_history INNER JOIN products INNER JOIN active_users ' +
+          'ON (bid_history.bid_product_id = products.product_id AND active_users.user_id = bid_history.bid_user_id) ' +
           'WHERE bid_history.bid_closed_date IS NULL AND ' +
           'products.product_id = ? ' +
           'ORDER BY bid_amount DESC';
@@ -44,10 +47,9 @@ module.exports.get = function(productId, bidId, callback) {
     if(err) {
       callback(err);
     } else {
-      var sql = 'SELECT bid_history.bid_id, bid_history.bid_amount, bid_history.bid_creation_date, ' +
-          'products.seller_name ' +
-          'FROM bid_history INNER JOIN products ' +
-          'ON (bid_history.bid_product_id = products.product_id) ' +
+      var sql = 'SELECT bid_history.*,  products.seller_name, active_users.user_login_user_name ' +
+          'FROM bid_history INNER JOIN products INNER JOIN active_users ' +
+          'ON (bid_history.bid_product_id = products.product_id AND active_users.user_id = bid_history.bid_user_id) ' +
           'WHERE bid_history.bid_closed_date IS NULL AND ' +
           'products.product_id = ? AND bid_history.bid_id = ? ' +
           'ORDER BY bid_amount DESC';
