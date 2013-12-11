3b34aedc24e5a70aedcfdebc119748c3a2994251
samy100 <samuel.matos@upr.edu>
2013-12-11 01:04:43 -0400

    Date Validation.

--- public/js/directives/register.js
+++ public/js/directives/register.js
@@ -68,9 +68,17 @@ angular.module('eMarketApp').directive('register', function(Restangular, Helper)
           $scope.register.billingCity = $scope.register.mailingCity;
           $scope.register.billingZipCode = $scope.register.mailingZipCode;
         }
-        $scope.register.cardExpDate = Helper.formatDate($scope.register.cardExpDate, 'yyyy-MM-dd');
+        var oldDateFormat = $scope.register.cardExpDate;
+        var newDateFormat = Helper.formatDate($scope.register.cardExpDate, 'yyyy-MM-dd');
+        if(new Date(newDateFormat) < new Date()) {
+          statusPopupMessage.text('Expiration Date already passed.');
+          statusPopup.popup('open');
+          return;
+        }
+        $scope.register.cardExpDate = newDateFormat;
         $.mobile.loading('show');
         Restangular.all('../register').post($scope.register).then(function() {
+          $scope.register.cardExpDate = oldDateFormat;
           $.mobile.loading('hide');
           statusPopupMessage.text('Registration successful. ' +
               'Your username is ' + $scope.register.username + '. ' +
