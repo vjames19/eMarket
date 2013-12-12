b6d0d799e71b72634a0eb188c6d4df2d1513c5cd
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2013-12-11 21:47:46 -0400

    Added submit payment in the front end

--- public/js/directives/proceedToCheckOut.js
+++ public/js/directives/proceedToCheckOut.js
@@ -6,12 +6,46 @@ angular.module('eMarketApp').directive('proceedToCheckout', function(User, CartI
     restrict: 'E',
     scope: {},
     replace: true,
-    controller: function($scope) {
+    controller: function($scope, $element) {
+
+      var page = $($element[0]);
+
+      var statusPopup = page.find('#submitPayment-statusPopup');
+      var statusPopupMessage = page.find('#submitPayment-statusPopupMessage');
 
       $scope.submit = function() {
+        statusPopup.off();
+        if(window._.isEmpty($scope.checkout)) {
+          statusPopupMessage.text('Please provide the requested information.');
+          statusPopup.popup('open');
+          return;
+        }
+        if(!$scope.checkout.selectedBank) {
+          $scope.checkout.selectedBank = null;
+        } else {
+          $scope.checkout.selectedCard = null;
+        }
         $.mobile.loading('show');
-        $.mobile.loading('hide');
-        $.mobile.changePage('#home-user');
+        User.me().all('submitPayment').post($scope.checkout).then(function() {
+          $.mobile.loading('hide');
+          statusPopupMessage.text('Payment Submitted Successfully');
+          statusPopup.popup('open');
+          statusPopup.on({
+            popupafterclose: function() {
+              $.mobile.changePage('#home-user');
+            }
+          });
+        }, function(err) {
+          $.mobile.loading('hide');
+          statusPopupMessage.text('Payment Not Submitted Successfully');
+          statusPopup.popup('open');
+          statusPopup.on({
+            popupafterclose: function() {
+              $.mobile.changePage('#shopping-cart');
+            }
+          });
+          console.log('Payment Error', err);
+        });
       };
 
     },
