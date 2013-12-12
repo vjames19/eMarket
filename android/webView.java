21c2fd45c3063fcd708a5a594154f109913ce8ac
Eduardo M. Breijo <eduardo.breijo@upr.edu>
2013-12-12 10:14:40 -0400

    Webview update

--- android/webView.java
+++ android/webView.java
@@ -21,17 +21,6 @@ public class MainActivity extends Activity {
 		webView.setWebViewClient(new myWebViewClient());
 	}
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
-			webView.goBack();
-			return true;
-		}
-		else {
-			return super.onKeyDown(keyCode, event); // Handles this onKeyDown
-		}
-	}
-
 	private class myWebViewClient extends WebViewClient {
 		@Override
 		public boolean shouldOverrideUrlLoading(WebView webView, String url) {
