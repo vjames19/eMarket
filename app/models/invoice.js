d8395fea568fa531dcadba6b8114eb791dc0bd43
samy100 <samuel.matos@upr.edu>
2013-12-12 04:20:58 -0400

    fixing bug in invoice getAll/get

--- app/models/invoice.js
+++ app/models/invoice.js
@@ -70,7 +70,7 @@ module.exports.getAll = function(userId, callback) {
     } else {
       var sql = 'SELECT invoice_id, SUM(invoice_item_sold_price) AS total, invoice_creation_date ' +
           'FROM invoice_history INNER JOIN user_info INNER JOIN invoice_item_history ' +
-          'ON (invoice_history.invoice_user_id=user_info.user_id AND invoice_id=invoice_item_id) ' +
+          'ON (invoice_history.invoice_user_id=user_info.user_id AND invoice_id=invoice_item_invoice_id) ' +
           'WHERE invoice_user_id = ? ' +
           'GROUP BY invoice_item_invoice_id, invoice_creation_date ' +
           'ORDER BY invoice_creation_date DESC';
@@ -89,7 +89,7 @@ module.exports.get = function(userId, invoiceId, callback) {
     } else {
       var sql = 'SELECT invoice_id, SUM(invoice_item_sold_price) AS total, invoice_creation_date ' +
           'FROM invoice_history INNER JOIN user_info INNER JOIN invoice_item_history ' +
-          'ON (invoice_history.invoice_user_id=user_info.user_id AND invoice_id=invoice_item_id) ' +
+          'ON (invoice_history.invoice_user_id=user_info.user_id AND invoice_id=invoice_item_invoice_id) ' +
           'WHERE invoice_user_id = ? AND invoice_id = ?' +
           'GROUP BY invoice_item_invoice_id, invoice_creation_date ' +
           'ORDER BY invoice_creation_date DESC';
